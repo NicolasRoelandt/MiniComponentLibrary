dfc00b59b4986bac185401d54802d994a37eebba
NicolasRoelandt <nicolasroelandt@fb.com>
2022-09-02 21:38:58 +0100

    ProgressBar and Select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,45 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const LENGTH = 370;
+
+const Outside = styled.div`
+  width: ${LENGTH}px;
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--borderRadius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+`;
+
+const Inside = styled.div`
+  width: var(--width);
+  height: var(--height);
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  background-color: ${COLORS.primary};
+`;
+
+const InsideWrapper = styled.div`
+  border-radius: 4px;
+  overflow : hidden;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const getHeight = () => {
+    switch(size){
+      case 'small': return 8;
+      case 'medium': return 12;
+      case 'large': return 16;
+    }
+  };
+  const padding = size === 'large' ? 4 : 0;
+  const borderRadius = size === 'large' ? 8 : 4;
+  return (<Outside role="progressbar" aria-valuenow={value} aria style={{'--padding': padding +'px', '--borderRadius': borderRadius +'px'}}>
+    <InsideWrapper>
+      <Inside style={{'--width': value +'%', '--height' : getHeight() +'px'}}/>
+    </InsideWrapper>
+  </Outside>);
 };
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,48 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Selector = styled.select`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  height: 43px;
+  padding: 12px 52px 12px 16px;
+  font-size: 16px;
+  border: unset;
+  /* for Firefox */
+  -moz-appearance: none;
+  /* for Chrome */
+  -webkit-appearance: none;
+  color: inherit;
+  width: fit-content;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+  :hover{
+    color : ${COLORS.black};
+  }
+`;
+
+
+const IconWrapper = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+     <Selector value={value} onChange={onChange}>
+       {children}
+     </Selector>
+     <IconWrapper><Icon id="chevron-down" size={24}  /></IconWrapper>
+    </Wrapper>
   );
 };
 
